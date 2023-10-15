bdb260987dbc6b02453743b6b008b6124690a83f
Venkatesan <venkatesangunaraj@gmail.com>
2023-10-15 16:03:27 +0530

    🚀 Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compress from 'koa-compress';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewares: [compress()], // Add the compress middleware
+  },
+  build: {
+    minify: 'terser', // Minify with Terser
+  },
 })
