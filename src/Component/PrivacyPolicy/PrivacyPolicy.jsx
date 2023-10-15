78a904bd8a18c65fa403e757549667a8a2cb2e31
VenkatMastercoder <venkatesangunaraj@gmail.com>
2023-10-15 17:00:24 +0530

    🐛 Updated Components

--- src/Component/PrivacyPolicy/PrivacyPolicy.jsx
+++
@@ -1,9 +0,0 @@
-const PrivacyPolicy = () => {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      PrivacyPolicy Page Comming Soon
-    </div>
-  );
-};
-
-export default PrivacyPolicy;
