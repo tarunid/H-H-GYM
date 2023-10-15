e5fef8f299f893634558edc84fa9b1c71d925f6b
Venkatesan <venkatesangunaraj@gmail.com>
2023-10-15 16:09:09 +0530

    🚀 Added Sitemap

---
+++ src/Component/Sitemap/Sitemap.jsx
@@ -0,0 +1,9 @@
+const Sitemap = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p className="text-black">sitemap</p>
+    </div>
+  )
+}
+
+export default Sitemap
