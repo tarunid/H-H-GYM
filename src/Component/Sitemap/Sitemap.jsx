8d34cf86f3905455c6180e1fda2123599db585a6
VenkatMastercoder <venkatesangunaraj@gmail.com>
2023-10-15 16:37:37 +0530

    ✨ Optimized

--- src/Component/Sitemap/Sitemap.jsx
+++
@@ -1,9 +0,0 @@
-const Sitemap = () => {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <p className="text-black">sitemap</p>
-    </div>
-  )
-}
-
-export default Sitemap
--- src/Component/TermAndCondition/TermCondition.jsx
+++
@@ -1,9 +0,0 @@
-const TermCondition = () => {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      TermCondition Page Comming Soon
-    </div>
-  );
-};
-
-export default TermCondition;
