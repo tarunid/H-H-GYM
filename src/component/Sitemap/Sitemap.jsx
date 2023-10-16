dbe6b5a326bed8bdf98cc1a289a5ce1e0eff400f
Venkatesan <venkatesangunaraj@gmail.com>
2023-10-16 21:28:12 +0530

    🚀 Added Sitemap

--- src/component/Sitemap/Sitemap.jsx
+++ src/component/Sitemap/Sitemap.jsx
@@ -1,8 +1,33 @@
 const Sitemap = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <p className="text-black">sitemap</p>
-    </div>
+    <>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+          <loc>https://hhfitnessstudio.com/</loc>
+          <lastmod>2023-10-12</lastmod>
+          <changefreq>daily</changefreq>
+          <priority>1.0</priority>
+        </url>
+        <url>
+          <loc>https://hhfitnessstudio.com/about</loc>
+          <lastmod>2023-10-10</lastmod>
+          <changefreq>weekly</changefreq>
+          <priority>0.8</priority>
+        </url>
+        <url>
+          <loc>https://hhfitnessstudio.com/contact</loc>
+          <lastmod>2023-10-11</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.7</priority>
+        </url>
+        <url>
+          <loc>https://hhfitnessstudio.com/gallery</loc>
+          <lastmod>2023-10-11</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.7</priority>
+        </url>
+      </urlset>
+    </>
   )
 }
 
