0d456b6af5a2e140543ee1256988fb797d93dd52
Venkatesan <venkatesangunaraj@gmail.com>
2023-11-03 22:29:30 +0530

    ✨ Optimized

--- src/component/Price/Price.jsx
+++ src/component/Price/Price.jsx
@@ -2,29 +2,35 @@ import "./Price.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { WhiteBg } from "../../assets/img";
+import axiosInstance from "../../interceptors/axiosInstance";
+import Loader from "../Loaded/Loader";
 
 const Price = () => {
-
+  const [isLoading, setIsLoading] = useState(false);
   const [priceCards, setPriceCards] = useState([]);
 
   useEffect(() => {
-    const url = 'https://hh-gym-backend-production.up.railway.app/api/price/all';
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setPriceCards(data.deserializedPriceCards);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+    fetchData();
   }, []);
+
+  let fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get("/price/all");
+      setPriceCards(response.data.deserializedPriceCards);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   priceCards.sort((a, b) => a.price - b.price);
 
   return (
     <>
-      <section className="py-14 bg-Price" style={{ backgroundImage: `url(${WhiteBg})`}}>
+      <section
+        className="py-14 bg-Price"
+        style={{ backgroundImage: `url(${WhiteBg})` }}>
         <div className="2xl:container mx-auto">
           <div className="grid grid-cols-1 w-[80%] mx-auto pb-14">
             <div className="flex flex-col justify-center items-center text-center my-1">
@@ -36,40 +42,45 @@ const Price = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-1 lg:grid-cols-4 w-[90%] mx-auto">
-            {priceCards.map((item) => (
-              <div
-                key={item.id}
-                className="flex flex-col justify-center items-center w-[90%] mx-auto bg-[#e8e8e8] text-[#000] rounded-lg my-5 py-5 hover:scale-105"
-              >
-                <div className="flex justify-between items-center w-[80%] mx-auto py-5">
-                  <h3 className="price-haeding">{item.tag}</h3>
+          {isLoading ? (
+            <div className="w-[100%] flex flex-col items-center justify-center">
+              <Loader />
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 lg:grid-cols-4 w-[90%] mx-auto">
+              {priceCards.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex flex-col justify-center items-center w-[90%] mx-auto bg-[#e8e8e8] text-[#000] rounded-lg my-5 py-5 hover:scale-105">
+                  <div className="flex justify-between items-center w-[80%] mx-auto py-5">
+                    <h3 className="price-haeding">{item.tag}</h3>
+                  </div>
+
+                  <ul className="w-[80%] mx-auto py-5 leading-loose ul-tag">
+                    {item.description.map((desc, index) => (
+                      <li key={index}>&#10003; {desc}</li>
+                    ))}
+                  </ul>
+
+                  <h2 className="flex justify-start items-center text-center w-[80%] mx-auto py-5">
+                    <p className="price">{item.price}</p>
+                    <span className="month-price">/ month</span>
+                  </h2>
+
+                  <div className="py-5">
+                    <button>
+                      <Link className="fancy" to="/contact">
+                        <span className="top-key"></span>
+                        <span className="textt">Enroll Now</span>
+                        <span className="bottom-key-1"></span>
+                        <span className="bottom-key-2"></span>
+                      </Link>
+                    </button>
+                  </div>
                 </div>
-
-                <ul className="w-[80%] mx-auto py-5 leading-loose ul-tag">
-                  {item.description.map((desc, index) => (
-                    <li key={index}>&#10003; {desc}</li>
-                  ))}
-                </ul>
-
-                <h2 className="flex justify-start items-center text-center w-[80%] mx-auto py-5">
-                  <p className="price">{item.price}</p>
-                  <span className="month-price">/ month</span>
-                </h2>
-
-                <div className="py-5">
-                  <button>
-                    <Link className="fancy" to="/contact">
-                      <span className="top-key"></span>
-                      <span className="textt">Enroll Now</span>
-                      <span className="bottom-key-1"></span>
-                      <span className="bottom-key-2"></span>
-                    </Link>
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
