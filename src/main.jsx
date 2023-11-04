2701b0b17becdb40cf73dc056efbcaaa211bcc98
VenkatMastercoder <venkatesangunaraj@gmail.com>
2023-11-04 10:35:20 +0530

    🚀 Changed Folder Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,10 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Error from "./component/Error/Error";
-import Loader from "./component/Loaded/Loader";
-import App from "./RootLayout/AppLayout";
-import Body from "./pages/Body";
+import routes from './routes/index'
 
-const LazyGalleryPage = lazy(() => import("./pages/Gallery"));
-const LazyAboutPage = lazy(() => import("./pages/About"));
-const LazyContactPage = lazy(() => import("./pages/Contact"));
-const LazyTermCondition = lazy(() => import("./component/TermAndCondition/TermCondition"));
-const LazyPrivacyPolicy = lazy(() => import("./component/PrivacyPolicy/PrivacyPolicy"));
-const LazySiteMap = lazy(() => import("./component/Sitemap/Sitemap"));
-
-let browerRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Body />,
-      },
-      {
-        path: "/gallery",
-        element: <Suspense fallback={<Loader />}><LazyGalleryPage /></Suspense>,
-      },
-      {
-        path: "/about",
-        element: <Suspense fallback={<Loader />}><LazyAboutPage /></Suspense>,
-      },
-      {
-        path: "/contact",
-        element: <Suspense fallback={<Loader />}><LazyContactPage /></Suspense>,
-      },
-      {
-        path: "/terms-and-condition",
-        element: <Suspense fallback={<Loader />}><LazyTermCondition /></Suspense>,
-      },
-      {
-        path: "/privacy-policy",
-        element: <Suspense fallback={<Loader />}><LazyPrivacyPolicy /></Suspense>,
-      },
-      {
-        path: "/sitemap",
-        element: <Suspense fallback={<Loader />}><LazySiteMap /></Suspense>,
-      }
-    ],
-    errorElement: <Error />,
-  },
-]);
+let browerRouter = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/routes/index.jsx
@@ -0,0 +1,53 @@
+import { lazy, Suspense } from 'react'
+
+import Error from "../component/Error/Error";
+import Loader from "../component/Loaded/Loader";
+import App from "../Layout/AppLayout";
+import Body from "../pages/Body";
+
+const LazyGalleryPage = lazy(() => import("../pages/Gallery"));
+const LazyAboutPage = lazy(() => import("../pages/About"));
+const LazyContactPage = lazy(() => import("../pages/Contact"));
+const LazyTermCondition = lazy(() => import("../component/PrivacyPolicy/PrivacyPolicy"));
+const LazyPrivacyPolicy = lazy(() => import("../component/PrivacyPolicy/PrivacyPolicy"));
+const LazySiteMap = lazy(() => import("../component/Sitemap/Sitemap"));
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/gallery",
+        element: <Suspense fallback={<Loader />}><LazyGalleryPage /></Suspense>,
+      },
+      {
+        path: "/about",
+        element: <Suspense fallback={<Loader />}><LazyAboutPage /></Suspense>,
+      },
+      {
+        path: "/contact",
+        element: <Suspense fallback={<Loader />}><LazyContactPage /></Suspense>,
+      },
+      {
+        path: "/terms-and-condition",
+        element: <Suspense fallback={<Loader />}><LazyTermCondition /></Suspense>,
+      },
+      {
+        path: "/privacy-policy",
+        element: <Suspense fallback={<Loader />}><LazyPrivacyPolicy /></Suspense>,
+      },
+      {
+        path: "/sitemap",
+        element: <Suspense fallback={<Loader />}><LazySiteMap /></Suspense>,
+      }
+    ],
+    errorElement: <Error />,
+  },
+]
+
+export default routes;
