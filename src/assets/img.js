30875f373a8f3eea570be70053ab5be4ec28d545
taruni d <tarunid.2003@gmail.com>
2024-03-18 23:19:11 +0530

    H&H-GYM

--- src/assets/img.js
+++ src/assets/img.js
@@ -3,14 +3,14 @@ export const Logo =
 
 export const Poster =
   "https://ik.imagekit.io/4z8covdo9/GYM/Poster.jpg?updatedAt=1696047890396";
+  
 export const CommomHero =
   "https://ik.imagekit.io/4z8covdo9/GYM/CommomHero.jpg?updatedAt=1696047886401";
 
 export const CommonTextWrapper =
   "https://ik.imagekit.io/4z8covdo9/GYM/CommonTextWrapper.svg?updatedAt=1696047886791";
-  export const CommonTextBlackWrapper =
+export const CommonTextBlackWrapper =
   "https://ik.imagekit.io/4z8covdo9/GYM/CommonTextWrapper.svg?updatedAt=1696047886791";
-
 
 export const BlackBg =
   "https://ik.imagekit.io/4z8covdo9/GYM/BlackBg.jpg?updatedAt=1696047886739";
@@ -33,9 +33,8 @@ export const video2 =
 export const WhiteBg =
   "https://ik.imagekit.io/4z8covdo9/GYM/WhiteBg.jpg?updatedAt=1696047893407";
 
-
 export const Dumbbell =
-  "https://ik.imagekit.io/4z8covdo9/GYM/Dumbbell.png?updatedAt=1696047886663";  
+  "https://ik.imagekit.io/4z8covdo9/GYM/Dumbbell.png?updatedAt=1696047886663";
 export const Heromuscle =
   "https://ik.imagekit.io/4z8covdo9/GYM/muscle.png?updatedAt=1696047890942";
 export const HeroFitness =
@@ -52,7 +51,6 @@ export const OfferIcon3 =
 export const OfferIcon4 =
   "https://ik.imagekit.io/4z8covdo9/GYM/OfferIcon4.png?updatedAt=1696047890607";
 
-
 export const Trainer =
   "https://ik.imagekit.io/4z8covdo9/GYM/Trainer.svg?updatedAt=1697342341336";
 export const DumbellColored =
@@ -62,7 +60,10 @@ export const Wellness =
 export const CommunitySupport =
   "https://ik.imagekit.io/4z8covdo9/GYM/CommunitySupport.svg?updatedAt=1697342341465";
 
-export const DefaultImg = "https://ik.imagekit.io/4z8covdo9/GYM/DefaultImg.jpg?updatedAt=1697343114246";
+export const DefaultImg =
+  "https://ik.imagekit.io/4z8covdo9/GYM/DefaultImg.jpg?updatedAt=1697343114246";
 
-export const AboutImg = "https://ik.imagekit.io/4z8covdo9/GYM/AboutImg.jpg?updatedAt=1696049895809";
-export const DumbbellPlain = "https://ik.imagekit.io/4z8covdo9/GYM/dumbbellPlain.png?updatedAt=1697348064378";
+export const AboutImg =
+  "https://ik.imagekit.io/4z8covdo9/GYM/AboutImg.jpg?updatedAt=1696049895809";
+export const DumbbellPlain =
+  "https://ik.imagekit.io/4z8covdo9/GYM/dumbbellPlain.png?updatedAt=1697348064378";
